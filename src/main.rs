813c9ed434039077bee15535e328984575baff9f
Sergey Potapov <blake131313@gmail.com>
2021-01-09 01:44:14 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,6 @@ mod report;
 mod benchmark;
 mod cli;
 
-use whatlang_corpora::Corpus;
-use whatlang::Lang;
-use rayon::prelude::*;
-
-use report::{LangReport, OverallReport, size};
-
 fn main() {
     cli::run();
 }
